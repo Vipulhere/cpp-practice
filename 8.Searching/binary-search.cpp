db6284c0699ee95e5ebd80e69e7be2466cb67cbc
Vipulhere <krvipul28@gmail.com>
2023-05-17 17:51:43 +0530

    codes on searching

--- 8.Searching/binary-search.cpp
+++ 8.Searching/binary-search.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int binarySearch(int arr[],int n,int key){
     int start=0,end=n-1;
     while(start<=end){
-        int mid=(start+end)/2;
+        // int mid=(start+end)/2;
+        int mid=start+(end-start)/2;
         if(arr[mid]==key){
             return mid;
         }else if(arr[mid]>key){
